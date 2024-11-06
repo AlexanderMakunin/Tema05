6f4d19a19bf5f200f3265ad6088aa9f47be2986e
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-11-06 19:05:59 +0100

    Terminar hasta la 5

--- src/main/java/com/alexandermakunin/tema05/Ejercicio4.java
+++ src/main/java/com/alexandermakunin/tema05/Ejercicio4.java
@@ -1,4 +1,48 @@
 package com.alexandermakunin.tema05;
 
 public class Ejercicio4 {
-}
+    /**
+     * contara las vocales que hay en el texto
+     * @param texto el texto
+     * @return devolvera la cantidad de vocales en el texto
+     */
+    public static int contadorVocales(String texto) {
+        int cuentaVocales = 0;
+            for (int j = 0; j < texto.length(); j++) {
+                char letra;
+                char[] array = texto.toCharArray();
+                letra = array[j];
+                switch (letra) {
+                    case 'a', 'e', 'i', 'o', 'u' -> cuentaVocales++;
+                }
+            }
+        return cuentaVocales;
+    }
+
+    /**
+     * contara las consonantes que hay en el texto
+     * @param texto el texto
+     * @return devolvera la cantidad de consonantes en el texto
+     */
+    public static int contadorConsonantes(String texto){
+        int cuentaConsonantes = 0;
+        for (int i = 0; i <= texto.length() -1; i++){
+            char letra;
+            char[] array = texto.toCharArray();
+            letra = array[i];
+            switch (letra){
+                case 'b','c','d','f','g','h','j','k','l','m','n','ñ','p','q','r','s','t','v','w','x','y','z' -> cuentaConsonantes++;
+            }
+        }
+        return cuentaConsonantes;
+    }
+
+    public static void main(String[] args) {
+        String texto = "hoLa Que Taleeñs";
+        String bajado = texto.toLowerCase();
+        String[] nombres = bajado.split("\\s+");
+        for (int j = 0; j < nombres.length; j++) {
+            System.out.println("En total hay: " + contadorVocales(nombres[j]) + " vocales, y " + contadorConsonantes(nombres[j]) + " consonantes");
+        }
+        }
+    }
--- src/main/java/com/alexandermakunin/tema05/Ejercicio5.java
+++ src/main/java/com/alexandermakunin/tema05/Ejercicio5.java
@@ -1,4 +1,22 @@
 package com.alexandermakunin.tema05;
 
 public class Ejercicio5 {
+    public static void main(String[] args) {
+        String texto = "hoLa Que Taleeñs";
+        String bajado = texto.toLowerCase();
+        String asdf;
+        String guardar = "";
+        int contador;
+        int cuenta = 0;
+        String[] nombres = bajado.split("\\s+");
+        for (int j = 0; j < nombres.length; j++){
+            asdf = nombres[j];
+            contador = asdf.length();
+            if (contador >= cuenta){
+                cuenta = contador;
+                guardar = asdf;
+            }
+        }
+        System.out.println("La palabra mas larga es: " + guardar + " ,que contiene: " + cuenta);
+    }
 }
