b89d2d0134eb70e6f6e724f9bc17f4aac886458c
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-11-19 17:12:59 +0100

    hacer actividades

---
+++ src/main/java/com/alexandermakunin/tema05/array/Ejercicio4.java
@@ -0,0 +1,36 @@
+package com.alexandermakunin.tema05.array;
+
+import java.util.Random;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+        double [] notas = new double[30];
+        for (int i = 0; i < notas.length; i++){
+            notas[i] = aleatorio.nextDouble(0,11);
+        }
+        double contador = 0;
+        for ( double numero : notas){
+            contador += numero;
+        }
+        double media = contador / notas.length;
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        String espacio = "";
+        String coma = ",";
+        for (int i = 0; i < notas.length; i++){
+            if (i == notas.length -1){
+                coma = "";
+            }
+            if (notas[i] >= 5) {
+                sb.append(espacio).append(String.format("%.4f", notas[i])).append(coma);
+            }
+            if (i == 0){
+                espacio = " ";
+            }
+        }
+        sb.append("}");
+        System.out.println(sb);
+        System.out.printf("La media es: %1.4f",media);
+    }
+}
---
+++ src/main/java/com/alexandermakunin/tema05/array/Ejercicio6.java
@@ -0,0 +1,36 @@
+package com.alexandermakunin.tema05.array;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Ejercicio6 {
+    public static float [] divisionArrays(int [] array1, int [] array2){
+        float [] arraySuma = new float[10];
+        for (int i = 0; i < array1.length;i++){
+            arraySuma[i] = (float) array1[i] / array2[i];
+        }
+        return arraySuma;
+    }
+    public static int [] sumaArrays(int [] array1, int [] array2){
+        int [] arraySuma = new int[10];
+        for (int i = 0; i < array1.length;i++){
+            arraySuma[i] = array1[i] + array2[i];
+        }
+        return arraySuma;
+    }
+    public static void main(String[] args) {
+        int [] array1 = new int[10];
+        int [] array2 = new int[10];
+        Random aleatorio = new Random();
+        for (int i = 0; i < array1.length; i++){
+            array1[i] = aleatorio.nextInt(1,11);
+        }
+        for (int i = 0; i < array2.length; i++){
+            array2[i] = aleatorio.nextInt(1,11);
+        }
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+        System.out.println(Arrays.toString(sumaArrays(array1,array2)));
+        System.out.println(Arrays.toString(divisionArrays(array1,array2)));
+    }
+}
