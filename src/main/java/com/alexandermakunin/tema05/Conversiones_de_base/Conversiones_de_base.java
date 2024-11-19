81faeea51fb56c1e4294c5f3dec318fd7a4d74b6
Alexander Makunin <amakunin7@gmail.com>
2024-11-19 01:47:29 +0100

    un par de cambios

---
+++ src/main/java/com/alexandermakunin/tema05/Conversiones_de_base/Conversiones_de_base.java
@@ -0,0 +1,20 @@
+//Aqui se mostrara el apartado del submenu de conversiones
+//(seguramente pa levi)
+package Conversiones_de_base;
+
+public class Conversiones_de_base {
+    public static void main(String[] args) {
+        System.out.println("\n**************************");
+        System.out.println("** CONVERSIONES DE BASE **");
+        System.out.println("**************************");
+        System.out.println("1. De decimal a binario");
+        System.out.println("2. De decimal a hexadecimal");
+        System.out.println("3. De binario a decimal");
+        System.out.println("4. De binario a hexadecimal");
+        System.out.println("5. De hexadecimal a binario");
+        System.out.println("6. De hexadecimal a decimal");
+        System.out.println("---------------------------");
+        System.out.println("0. Volver al menú principal");
+        System.out.print("Elija una opción: ");
+    }
+}
---
+++ src/main/java/com/alexandermakunin/tema05/lib/IO.java
@@ -0,0 +1,5 @@
+//Si quereis añadir una wea o lo que sea aqui lo teneis
+package lib;
+
+public class IO {
+}
