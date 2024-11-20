338c46fea24055cfd661594ce7472510fd6dbc15
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-11-20 15:26:17 +0100

    eliminar una cosilla de la 5

--- src/main/java/com/alexandermakunin/tema05/arrays/Ejercicio5.java
+++ src/main/java/com/alexandermakunin/tema05/arrays/Ejercicio5.java
@@ -2,7 +2,6 @@ package com.alexandermakunin.tema05.arrays;
 
 import java.util.Arrays;
 import java.util.Scanner;
-//No entendi lol
 public class Ejercicio5 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
