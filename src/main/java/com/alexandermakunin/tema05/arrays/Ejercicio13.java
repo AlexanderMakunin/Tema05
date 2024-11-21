8ddeb70056c18dae0c0eebfd5153a9d9478d102a
Alexander Makunin <amakunin7@gmail.com>
2024-11-22 00:08:20 +0100

    ya chambie grasas en la 13

--- src/main/java/com/alexandermakunin/tema05/arrays/Ejercicio13.java
+++ src/main/java/com/alexandermakunin/tema05/arrays/Ejercicio13.java
@@ -6,27 +6,28 @@ import java.util.Random;
 public class Ejercicio13 {
     public static void main(String[] args) {
         int[] v = new int[50];
+        int contador = 0;
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         Random aleatorio = new Random();
         for (int i = 0; i < v.length; i++){
             v[i] = aleatorio.nextInt(1,51);
         }
-        int[] p = new int[v.length];
-        System.out.println(Arrays.toString(v));
         if (v.length >= 50){
-            for (int i = 0; i < v.length;i++){
-                System.out.println(v[i]);
-                p[i] = v[i];
-                if (p[i] % 2 == 0){
-                    sb.append(p[i]);
-                    sb.append(", ");
+            for (int numero : v) {
+                if (numero % 2 == 0) {
+                    contador++;
                 }
             }
-            sb.append("]");
-            String texto = sb.toString();
-            texto = texto.replace(", ]","]");
-            System.out.println(texto);
+            int[] p = new int[contador];
+            contador = 0;
+            for (int numero : v) {
+                if (numero % 2 == 0) {
+                    p[contador] = numero;
+                    contador++;
+                }
+            }
+            System.out.println(Arrays.toString(p));
         }
     }
 }
