73bc6ff10ffd989151e25d9b198f6d0838ee4776
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-11-26 15:29:48 +0100

    mejorar ligeramente busquedaLinealOrr

--- src/main/java/com/alexandermakunin/tema05/Array_Busca_Ordenacion/busquedaLinealOrd.java
+++ src/main/java/com/alexandermakunin/tema05/Array_Busca_Ordenacion/busquedaLinealOrd.java
@@ -3,13 +3,14 @@ package com.alexandermakunin.tema05.Array_Busca_Ordenacion;
 public class busquedaLinealOrd {
     public static void main(String[] args) {
         int[] array = {1,20,100,100,1000};
-        int buscar = 100;
+        int buscar = 101;
         for (int i = 0; i <= array.length-1;i++){
             if (buscar == array[i]){
                 System.out.println("se ha encontrado en la posicion: " + i);
                 break;
-            } else if (buscar < array[i] || i == array.length-1){
+            } else if (buscar < array[i]){
                 System.out.println("no se ha encontrado");
+                break;
             }
         }
     }
