495e53088820a70480a9bfafad66aeea91a03a02
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-12-05 20:11:47 +0100

    continuar con el tres en raya

--- src/main/java/com/alexandermakunin/tema05/Matrices/Ejercicio3.java
+++ src/main/java/com/alexandermakunin/tema05/Matrices/Ejercicio3.java
@@ -24,19 +24,19 @@ public class Ejercicio3 {
         for (int i = 0; i < tablero.length;i++){
             sb.append("|---|---|---|\n");
             for (int j = 0; j < tablero[i].length;j++){
-                sb.append("| ").append(tablero[i][i]);
+                sb.append("| ").append(tablero[i][i].equals(Ficha.NONE) ? " " : sb.append(" "));
             }
         }
         sb.append("|---|---|---|\n");
         return sb.toString();
 
     }
-    public static boolean esJugadaValida (char fila, int columna){
-        int filaReal = fila - 'A';
-        int columnaReal = columna - 1;
+    public static boolean esJugadaValida (char fila, char columna){
+        int filaReal = getFilaReal(fila);
+        int columnaReal = getColumnaReal(columna);
         return tablero[filaReal][columnaReal] != null;
     }
-    public static void play (Ficha[][] tablero, Jugador turnoActual) {
+    public static void play () {
         char fila = ' ';
         char columna = ' ';
         leer = new Scanner(System.in);
@@ -58,18 +58,22 @@ public class Ejercicio3 {
                 System.err.println("Debe indicar la fila (letra) y la columna (número), Ej: A2");
                 continue;
             }
-            valido = esJugadaValida(fila, Character.getNumericValue(columna));
+            valido = esJugadaValida(fila, columna);
             if (!valido) {
                 System.err.println("Esa posicion ya esta ocupada");
             }
         }while (!valido);
         tablero[getFilaReal(fila)][getColumnaReal(columna)] = getFichaJugador(turnoActual);
+        tableroToString(tablero);
+        if (esJugadaGanadora(tablero, getFichaJugador(turnoActual))){
+            System.out.println("Jugador: " + turnoActual + " ha ganado la partida");
+        }
     }
     public static void reset () {
         Random turnoJugador = new Random();
         for (int i = 0; i < tablero.length;i++){
             for (int j = 0; j < tablero[i].length;j++){
-                tablero[i][j] = null;
+                tablero[i][j] = Ficha.NONE;
             }
         }
         Jugador[] valoresJugador = Jugador.values();
@@ -77,7 +81,7 @@ public class Ejercicio3 {
     }
 
     public enum Ficha {
-        O, X
+        O, X, NONE
     }
 
     public enum Jugador {
@@ -90,11 +94,58 @@ public class Ejercicio3 {
     private static Ficha[][] tablero;
     private static Scanner leer;
 
+    private static boolean esJugadaGanadora(Ficha[][] tablero, Ficha ficha) {
+        int contadorHorizontal;
+        int contadorVertical;
+        int contadorDiagonal1 = 0;
+        int contadorDiagonal2 = 0;
+        for (int i = 0; i < tablero.length; i++) {
+            contadorHorizontal = 0;
+            contadorVertical = 0;
+            for (int j = 0; j < tablero[i].length; j++){
+                if (tablero[i][j].equals(ficha)){
+                    contadorHorizontal++;
+                }
+                if (tablero[j][i].equals(ficha)){
+                    contadorVertical++;
+                }
+                if (i == j && tablero[i][j].equals(ficha)){
+                    contadorDiagonal1++;
+                }
+                if (j == tablero.length -1 -i && tablero[i][j].equals(ficha)){
+                    contadorDiagonal2++;
+                }
+            }
+            if (contadorHorizontal == tablero[i].length || contadorVertical == tablero.length) {
+                return true;
+            }
+            // Comprobar diagonales
+            if (contadorDiagonal1 == tablero.length || contadorDiagonal2 == tablero.length){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         tablero = new Ficha[FILAS][COLUMNAS];
+        leer = new Scanner(System.in);
+        boolean partidaFinalizada = true;
+        boolean hayGanador;
         reset();
-        play(tablero,turnoActual);
-        play(tablero,turnoActual);
+        do{
+            play();
+            hayGanador = esJugadaGanadora(tablero, getFichaJugador(turnoActual));
+            if (!hayGanador || !partidaFinalizada) {
+                turnoActual = siguienteTurno();
+            }
+            partidaFinalizada = hayGanador;
+        } while (!partidaFinalizada);
+    }
+
+    private static Jugador siguienteTurno() {
+        Jugador[] jugadores = Jugador.values();
+        return jugadores[(turnoActual.ordinal() + 1) % jugadores.length];
     }
 
 }
