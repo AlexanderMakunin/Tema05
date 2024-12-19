6fbe3a2e167e9ad5c7b3453328989133aa61fc64
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-12-19 20:09:07 +0100

    pa casita una wea el tesoro

--- src/main/java/com/alexandermakunin/tema05/Matrices/Config.java
+++ src/main/java/com/alexandermakunin/tema05/Matrices/Config.java
@@ -8,6 +8,11 @@ public class Config {
     public static final int CANTIDAD_OBSTACULOS = FILAS - 1;
     public static final int CANTIDAD_NPC = Math.min(FILAS * COLUMNAS / 20, 4);
     public static final int CANTIDAD_HABILIDADES = CANTIDAD_NPC;
+    public static final char UP = 'w';
+    public static final char LEFT = 'a';
+    public static final char RIGHT = 'd';
+    public static final char DOWN = 's';
+    public static final char DIG = 'e';
 
     // Constantes para sprites
     public static final String SPRITE_JUGADOR = "@";
--- src/main/java/com/alexandermakunin/tema05/Matrices/Ejercicio5.java
+++ src/main/java/com/alexandermakunin/tema05/Matrices/Ejercicio5.java
@@ -1,8 +1,15 @@
 package com.alexandermakunin.tema05.Matrices;
 
+import com.alexandermakunin.lib.ANSI;
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
+import javax.swing.*;
 import java.util.Random;
 
-public class Ejercicio5 {
+public class Ejercicio5 implements NativeKeyListener {
     public enum Celda {
         VACIA(Config.SPRITE_VACIA),
         NPC(Config.SPRITE_NPC),
@@ -23,10 +30,17 @@ public class Ejercicio5 {
         }
     }
 
+    public enum Movimiento {
+        UP, DOWN, LEFT, RIGHT, NONE, DIG
+    }
+
     private static Random random;
     private static Celda[][] mapa;
     private static Posicion[] posicionesLibres;
+    private static Posicion posicionJugador;
+    private static Celda celdaDestino;
     private static int cantidadPosicionesLibres;
+    private static int usosRestantes;
 
 
     public static void main(String[] args) {
@@ -35,6 +49,15 @@ public class Ejercicio5 {
         cantidadPosicionesLibres = 0;
         posicionesLibres = new Posicion[Config.FILAS * Config.COLUMNAS];
 
+        try {
+            GlobalScreen.registerNativeHook();
+            GlobalScreen.addNativeKeyListener(new Ejercicio5());
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+            System.exit(1);
+        }
+
         reset();
 
         System.out.println(mapaToString(mapa));
@@ -42,6 +65,7 @@ public class Ejercicio5 {
 
     public static void reset() {
         generarMapa();
+        usosRestantes = 3;
     }
 
 
@@ -56,26 +80,31 @@ public class Ejercicio5 {
             }
         }
 
-        for (int i = 0; i < Config.CANTIDAD_NPC;i++){
-            generarCeldaAleatoria(Celda.NPC);
-        }
-
-        for (int i = 0; i < Config.CANTIDAD_OBSTACULOS;i++){
+        // Generamos obstáculos
+        for (int i = 0; i < Config.CANTIDAD_OBSTACULOS; i++) {
             generarCeldaAleatoria(Celda.OBSTACULO);
         }
 
-        for (int i = 0; i < Config.CANTIDAD_HABILIDADES;i++){
+        // Generamos NPC
+        for (int i = 0; i < Config.CANTIDAD_NPC; i++) {
+            generarCeldaAleatoria(Celda.NPC);
+        }
+
+        // Generamos HABILIDADES
+        for (int i = 0; i < Config.CANTIDAD_HABILIDADES; i++) {
             generarCeldaAleatoria(Celda.HABILIDAD);
         }
+
         generarCeldaAleatoria(Celda.TESORO);
-        generarCeldaAleatoria(Celda.JUGADOR);
+
+        posicionJugador = generarCeldaAleatoria(Celda.JUGADOR);
 
     }
 
-    public static void generarCeldaAleatoria(Celda celda) {
+    public static Posicion generarCeldaAleatoria(Celda celda) {
         // Si no hay posiciones libres, salimos
         if (cantidadPosicionesLibres == 0) {
-            return;
+            return null;
         }
         // Generamos un índice aleatorio
         int indiceAleatorio = random.nextInt(cantidadPosicionesLibres);
@@ -87,22 +116,115 @@ public class Ejercicio5 {
         posicionesLibres[indiceAleatorio] = posicionesLibres[cantidadPosicionesLibres];
         // Finalmente, ponemos la celda que hemos recibido como parámetro en la posición indicada
         mapa[posicion.fila][posicion.columna] = celda;
+        return posicion;
     }
 
 
     public static String mapaToString(Celda[][] mapa) {
         StringBuilder sb = new StringBuilder();
-        sb.append(Celda.OBSTACULO.sprite.repeat(mapa[0].length+2)).append("\n");
+        sb.append(Celda.OBSTACULO.sprite.repeat(mapa[0].length + 2)).append("\n");
         for (Celda[] fila : mapa) {
-            sb.append(Celda.OBSTACULO.sprite);
+            sb.append(Celda.OBSTACULO);
             for (Celda celda : fila) {
                 sb.append(celda);
             }
-            sb.append(Celda.OBSTACULO.sprite);
-            sb.append("\n");
+            sb.append(Celda.OBSTACULO).append("\n");
         }
-        sb.append(Celda.OBSTACULO.sprite.repeat(mapa[0].length+2));
+        sb.append(Celda.OBSTACULO.sprite.repeat(mapa[0].length + 2)).append("\n");
         return sb.toString();
+    }
+
+    @Override
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        char tecla = e.getKeyChar();
+        System.out.println("\n");
+        Movimiento movimiento = switch (tecla) {
+            case Config.UP -> Movimiento.UP;
+            case Config.DOWN -> Movimiento.DOWN;
+            case Config.LEFT -> Movimiento.LEFT;
+            case Config.RIGHT -> Movimiento.RIGHT;
+            case Config.DIG -> Movimiento.DIG;
+            default -> Movimiento.NONE;
+        };
+        if (!movimiento.equals(Movimiento.NONE)) {
+            mover(movimiento);
+            ANSI.clearScreen();
+            System.out.println(mapaToString(mapa));
+            System.out.println("Key Typed: " + e.getKeyChar());
+        }
+    }
+
+    public static void dig() {
+        if (!mapa[posicionJugador.fila][posicionJugador.columna].equals(Celda.EXCAVADA)) {
+            mapa[posicionJugador.fila][posicionJugador.columna] = Celda.EXCAVADA;
+        }
+    }
+
+    public static void DIG(Movimiento movimiento) {
+        if (movimiento.equals(Movimiento.DIG)){
+            dig();
+            return;
+        }
+    }
+
+    public static void mover(Movimiento movimiento) {
+        Posicion movimientoDestino = new Posicion(posicionJugador.fila, posicionJugador.columna);
+
+        Celda destino = mapa[posicionJugador.fila][posicionJugador.columna];
+        switch (movimiento){
+            case UP -> {
+                if (posicionJugador.fila > 0) {
+                    destino = mapa[posicionJugador.fila - 1][posicionJugador.columna];
+                    movimientoDestino.fila -= 1;
+                }
+            }
+            case DOWN -> {
+                if (posicionJugador.fila < mapa.length - 1) {
+                    destino = mapa[posicionJugador.fila + 1][posicionJugador.columna];
+                    movimientoDestino.fila += 1;
+                }
+            }
+            case LEFT -> {
+                if (posicionJugador.columna > 0) {
+                    destino = mapa[posicionJugador.fila][posicionJugador.columna - 1];
+                    movimientoDestino.columna -= 1;
+                }
+            }
+            case RIGHT -> {
+                if (posicionJugador.columna < mapa[0].length - 1) {
+                    destino = mapa[posicionJugador.fila][posicionJugador.columna + 1];
+                    movimientoDestino.columna += 1;
+                }
+            }
+
+        }
+        switch (destino) {
+            case OBSTACULO -> {
+                return;
+            }
+            case NPC -> interaccionNPC();
+            default -> {
+                mapa[posicionJugador.fila][posicionJugador.columna] = celdaDestino;
+                //guardamos la celda destino
+                celdaDestino = mapa[movimientoDestino.fila][movimientoDestino.columna];
+                if (destino.equals(Celda.HABILIDAD)){
+                    celdaDestino = Celda.VACIA;
+                } else {
+                    celdaDestino = mapa[movimientoDestino.fila][movimientoDestino.columna];
+                }
+                mapa[movimientoDestino.fila][movimientoDestino.columna] = Celda.JUGADOR;
+                posicionJugador.fila = movimientoDestino.fila;
+                posicionJugador.columna = movimientoDestino.columna;
+            }
+        }
+    }
+
+    public static void interaccionNPC() {
+
+    }
+
+    public static void interacionHabilidad() {
+
     }
 
     public static class Posicion {
